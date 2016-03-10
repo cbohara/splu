79b6a9a7810ac4abbeeb4b670f423a678ea4f399
Charlie O'Hara <charlie.b.ohara@gmail.com>
2016-03-10 20:09:54 +0000

    building about page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Splu::Application.routes.draw do
 
   resources :quotes
 
+  get 'about', to: 'quotes#about'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
